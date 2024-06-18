2c137efc937f8a6cfa5297706b5cb676dbbd1d21
tervingo <keltivas@gmail.com>
2024-06-18 21:04:44 +0200

    Changing csv & pdf name to iv-en.xxx

--- backend/constants.py
+++ backend/constants.py
@@ -1,12 +1,12 @@
 paths = {
-    "AYUR_PDF_PATH" : "C:\\Users\\j4alo\\Dropbox\\Lenguas\\Ilven\\Ilven-Inglisen_kiskirtus.pdf",
-    "AMLENAI_PDF_PATH" : "C:\\Users\\administrator\\Dropbox\\Lenguas\\Ilven\\Ilven-Inglisen_kiskirtus.pdf",
+    "AYUR_PDF_PATH" : "C:\\Users\\j4alo\\Dropbox\\Lenguas\\Ilven\\iv-en.pdf",
+    "AMLENAI_PDF_PATH" : "C:\\Users\\administrator\\Dropbox\\Lenguas\\Ilven\\iv-en.pdf",
 
     "AYUR_ROOT_PDF_PATH" : "C:\\Users\\j4alo\\Dropbox\\Lenguas\\Ilven\\konota.pdf",
     "AMLENAI_ROOT_PDF_PATH" : "C:\\Users\\administrator\\Dropbox\\Lenguas\\Ilven\\konota.pdf",
 
-    "AYUR_CSV_PATH" : "C:\\Users\\j4alo\\Dropbox\\Lenguas\\Ilven\\Ilven-Inglisen_kiskirtus.csv",
-    "AMLENAI_CSV_PATH" : "C:\\Users\\administrator\\Dropbox\\Lenguas\\Ilven\\Ilven-Inglisen_kiskirtus.csv",
+    "AYUR_CSV_PATH" : "C:\\Users\\j4alo\\Dropbox\\Lenguas\\Ilven\\iv-en.csv",
+    "AMLENAI_CSV_PATH" : "C:\\Users\\administrator\\Dropbox\\Lenguas\\Ilven\\iv-en.csv",
 
     "AYUR_ROOT_CSV_PATH" : "C:\\Users\\j4alo\\Dropbox\\Lenguas\\Ilven\\konota.csv",
     "AMLENAI_ROOT_CSV_PATH" : "C:\\Users\\administrator\\Dropbox\\Lenguas\\Ilven\\konota.csv",
