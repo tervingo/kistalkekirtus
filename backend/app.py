d270a0ebf29cdfa2043e8ca5f8bf7fe4cfff85f0
tervingo <keltivas@gmail.com>
2024-12-11 21:08:47 +0100

    fixed app.py for Render

--- backend/app.py
+++ backend/app.py
@@ -27,6 +27,9 @@ def create_app():
 
     return app
 
+# Create an app instance for Gunicorn to find
+app = create_app()
+
 if __name__ == '__main__':
     app = create_app()
     app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=True)
